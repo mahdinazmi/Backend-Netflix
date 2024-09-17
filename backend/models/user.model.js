eb07fc75456bbf83a1e7174087ff89bfc24ce383
mahdi <mahdiforwork@gmail.com>
2024-09-17 19:13:45 +0330

    Netflix Backend

---
+++ backend/models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	image: {
+		type: String,
+		default: "",
+	},
+	searchHistory: {
+		type: Array,
+		default: [],
+	},
+});
+
+export const User = mongoose.model("User", userSchema);
